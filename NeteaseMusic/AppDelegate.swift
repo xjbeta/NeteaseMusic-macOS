06ccf94a5fd7e66b0003ef769f18aa8cafb088a2
xjbeta <xjbeta.tiny@gmail.com>
2019-03-28 21:27:57 +0800

    Init.

---
+++ NeteaseMusic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NeteaseMusic
+//
+//  Created by xjbeta on 2019/3/28.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NeteaseMusic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NeteaseMusic
+//
+//  Created by xjbeta on 2019/3/28.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
