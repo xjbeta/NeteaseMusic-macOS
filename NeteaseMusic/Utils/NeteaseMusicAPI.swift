f32fc24f70c18d66a45c909b9e481816cc9c315e
xjbeta <xjbeta.tiny@gmail.com>
2019-04-05 13:04:46 +0800

    SubString extension.  Login status, user info API.

--- NeteaseMusic/Utils/NeteaseMusicAPI.swift
+++ NeteaseMusic/Utils/NeteaseMusicAPI.swift
@@ -7,7 +7,55 @@
 //
 
 import Cocoa
+import Alamofire
+import Marshal
+import PromiseKit
 
 class NeteaseMusicAPI: NSObject {
-
+    struct User {
+        var nickname: String
+        var userId: Int
+        var avatarImage: NSImage?
+    }
+    
+    func isLogin() -> Promise<(Bool)> {
+        return userInfo().map {
+            $0.userId != -1
+        }
+    }
+    
+    
+    func userInfo() -> Promise<(User)> {
+        return Promise { resolver in
+            Alamofire.request("https://music.163.com").responseString {
+                let gUserStr = $0.value?.subString(from: "var GUser={", to: "};").replacingOccurrences(of: "\"", with: "")
+                
+                var user = User(nickname: "", userId: -1, avatarImage: nil)
+                gUserStr?.split(separator: ",").map(String.init).forEach {
+                    let pars = $0.split(separator: ":", maxSplits: 1).map(String.init)
+                    guard pars.count == 2, let key = pars.first else { return }
+                    let value = pars[1]
+                    switch key {
+                    case "userId":
+                        user.userId = Int(value) ?? -1
+                    case "nickname":
+                        user.nickname = value
+                    case "avatarUrl":
+                        let s = value.replacingOccurrences(of: "http://", with: "https://")
+                        if let u = URL(string: s) {
+                            user.avatarImage = NSImage(contentsOf: u)
+                        }
+                    default:
+                        break
+                    }
+                }
+                resolver.fulfill(user)
+            }
+        }
+    }
+    
+    
+    
+    
+    
 }
