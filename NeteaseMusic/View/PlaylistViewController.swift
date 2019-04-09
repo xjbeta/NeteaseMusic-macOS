b39b441838c3afd487e84bc6a8a645bcff4ff0ab
xjbeta <xjbeta.tiny@gmail.com>
2019-04-09 14:07:28 +0800

    Init playlist view.

---
+++ NeteaseMusic/View/PlaylistViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlaylistViewController.swift
+//  NeteaseMusic
+//
+//  Created by xjbeta on 2019/4/9.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class PlaylistViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
