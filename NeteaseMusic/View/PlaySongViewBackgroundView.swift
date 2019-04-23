32d8b8ad2749500fcc6dce031e592818a07d648b
xjbeta <xjbeta.tiny@gmail.com>
2019-04-23 22:59:53 +0800

    Fix click action through view.

---
+++ NeteaseMusic/View/PlaySongViewBackgroundView.swift
@@ -0,0 +1,24 @@
+//
+//  PlaySongViewBackgroundView.swift
+//  NeteaseMusic
+//
+//  Created by xjbeta on 2019/4/23.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class PlaySongViewBackgroundView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+    
+    override func mouseDown(with event: NSEvent) {
+        
+        
+    }
+}
