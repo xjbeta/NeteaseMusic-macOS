57281c43d74c374c5dc1272aee919080f59a096b
xjbeta <xjbeta.tiny@gmail.com>
2019-05-09 22:18:47 +0800

    FM storyboard.

---
+++ NeteaseMusic/View/FMViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FMViewController.swift
+//  NeteaseMusic
+//
+//  Created by xjbeta on 2019/5/9.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class FMViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
