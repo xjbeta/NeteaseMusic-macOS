8d8376715f4e33d61756c0b0f319f12d1e349a09
xjbeta <xjbeta.tiny@gmail.com>
2019-08-15 21:32:35 +0800

    Load user playlist, add track to playlist.

--- NeteaseMusic/View/SongButtonsPopUpViewController.swift
+++ NeteaseMusic/View/SongButtonsPopUpViewController.swift
@@ -12,16 +12,31 @@ class SongButtonsPopUpViewController: NSViewController {
     @IBOutlet weak var tableView: NSTableView!
     
     @IBAction func selected(_ sender: NSTableView) {
-        guard let id = playlists[safe: tableView.selectedRow]?.id else { return }
+        guard let playlistId = playlists[safe: tableView.selectedRow]?.id else { return }
+        
+        PlayCore.shared.api.playlistTracks(add: true, [trackId], to: playlistId).done {
+            print("Added song: \(self.trackId) to playlist: \(playlistId).")
+            }.catch {
+                print($0)
+        }
         dismiss(self)
-        complete?(id)
     }
     
     var playlists = [Playlist]()
     var complete: ((Int) -> Void)?
+    var trackId = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func loadPlaylists() {
+        PlayCore.shared.api.userPlaylist().done(on: .main) { [weak self] in
+            self?.playlists = $0
+            self?.tableView.reloadData()
+            }.catch {
+                print("Load user playlist error: \($0).")
+        }
     }
 }
 
