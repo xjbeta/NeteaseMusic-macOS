100ea05e1f910086ccf9997e9d2a10ef9e82c1f3
xjbeta <xjbeta.tiny@gmail.com>
2019-08-22 23:07:31 +0800

    Add cookie.

--- NeteaseMusic/AppDelegate.swift
+++ NeteaseMusic/AppDelegate.swift
@@ -15,6 +15,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        
+        let cookie = HTTPCookie(properties: [.domain : "music.163.com",
+                                        .name: "os",
+                                        .value: "pc",
+                                        .path: "/"])!
+        HTTPCookieStorage.shared.setCookie(cookie)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
