39455f8189083252a48b97f7a5fb18269e57daf1
xjbeta <xjbeta.tiny@gmail.com>
2019-10-19 11:43:00 +0800

    Preferences hotKeys.

--- NeteaseMusic/Utils/Preferences.swift
+++ NeteaseMusic/Utils/Preferences.swift
@@ -12,6 +12,44 @@ class Preferences: NSObject {
     static let shared = Preferences()
     
     private override init() {
+        
+        let commandStr = "\(NSEvent.ModifierFlags(arrayLiteral: [.command]).rawValue)"
+        let commandOptionStr = "\(NSEvent.ModifierFlags(arrayLiteral: [.command, .option]).rawValue)"
+        let commandControlStr = "\(NSEvent.ModifierFlags(arrayLiteral: [.command, .control]).rawValue)"
+        
+        defaultPreferencesHotKeys =
+            // Space
+            [.play: ["keyCode": "49"],
+             // ⌥⌘Space
+                .playGlobal: ["flags": commandOptionStr, "keyCode": "49"],
+                // ⌘←
+                .pre: ["flags": commandStr, "keyCode": "123"],
+                // ⌥⌘←
+                .preGlobal: ["flags": commandOptionStr, "keyCode": "123"],
+                // ⌘→
+                .next: ["flags": commandStr, "keyCode": "124"],
+                // ⌥⌘→
+                .nextGlobal: ["flags": commandOptionStr, "keyCode": "124"],
+                // ⌘↑
+                .volumeUp: ["flags": commandStr, "keyCode": "126"],
+                // ⌥⌘↑
+                .volumeUpGlobal: ["flags": commandOptionStr, "keyCode": "126"],
+                // ⌘↓
+                .volumeDown: ["flags": commandStr, "keyCode": "125"],
+                // ⌥⌘↓
+                .volumeDownGlobal: ["flags": commandOptionStr, "keyCode": "125"],
+                // ⌘L
+                .like: ["flags": commandStr, "keyCode": "37"],
+                // ⌥⌘L
+                .likeGlobal: ["flags": commandOptionStr, "keyCode": "37"],
+                // ⌘R
+                .lyric: ["flags": commandStr, "keyCode": "15"],
+                // ⌥⌘R
+                .lyricGlobal: ["flags": commandOptionStr, "keyCode": "15"],
+                // ⌃⌘M
+                .mini: ["flags": commandControlStr, "keyCode": "46"],
+                .miniGlobal: ["flags": "", "keyCode": ""]]
+        
     }
     let prefs = UserDefaults.standard
     let keys = PreferenceKeys.self
@@ -21,6 +59,32 @@ class Preferences: NSObject {
     }
     enum ShuffleMode: Int {
         case noShuffle, shuffleItems, shuffleAlbums
+    }
+    
+    var defaultPreferencesHotKeys: [PreferencesKeyEquivalents: [String: String]]
+    
+    var hotKeys: [PreferencesKeyEquivalents : [String : String]] {
+        get {
+            if let hotKeys = defaults(.hotKeys) as? [String: [String: String]] {
+                var dic: [PreferencesKeyEquivalents : [String : String]] = [:]
+                hotKeys.forEach {
+                    if let key = PreferencesKeyEquivalents(rawValue: $0.key) {
+                        dic[key] = $0.value
+                    }
+                }
+                return dic
+            } else {
+                return defaultPreferencesHotKeys
+            }
+        }
+        
+        set {
+            var dic: [String : [String : String]] = [:]
+            newValue.forEach {
+                dic[$0.key.rawValue] = $0.value
+            }
+            defaultsSet(dic, forKey: .hotKeys)
+        }
     }
     
     var repeatMode: RepeatMode {
@@ -89,4 +153,5 @@ enum PreferenceKeys: String {
     case mute
     case repeatMode
     case shuffleMode
+    case hotKeys
 }
