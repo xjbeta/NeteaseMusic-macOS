6f614f1cde12423816bdc9aef5572129db0c99da
xjbeta <xjbeta.tiny@gmail.com>
2020-01-02 21:39:24 +0800

    TrackTableView Menu.

--- NeteaseMusic/View/PlaylistViewController.swift
+++ NeteaseMusic/View/PlaylistViewController.swift
@@ -10,7 +10,7 @@ import Cocoa
 import PromiseKit
 
 class PlaylistViewController: NSViewController {
-
+    
     @IBOutlet weak var playAllButton: NSButton!
     @IBOutlet weak var subscribeButton: SubscribeButton!
     @IBOutlet weak var coverImageView: NSImageView!
@@ -46,17 +46,128 @@ class PlaylistViewController: NSViewController {
         api.subscribe(id, unSubscribe: subscribed, type: playlistType)
             .ensure(on: .main) {
                 sender.isEnabled = true
-            }.done {
-                sender.subscribed = !sender.subscribed
-                guard let vc = self.view.window?.windowController?.contentViewController as? MainViewController else { return }
-                vc.children.compactMap {
-                    $0 as? SidebarViewController
-                    }.first?.updatePlaylists()
-                print("playlist subscribe / unsubscribe success")
-            }.catch {
-                print($0)
+        }.done {
+            sender.subscribed = !sender.subscribed
+            guard let vc = self.view.window?.windowController?.contentViewController as? MainViewController else { return }
+            vc.children.compactMap {
+                $0 as? SidebarViewController
+            }.first?.updatePlaylists()
+            print("playlist subscribe / unsubscribe success")
+        }.catch {
+            print($0)
         }
     }
+    
+        // MARK: - Playlist Menu
+    @IBOutlet var playlistMenu: NSMenu!
+    
+    @IBOutlet weak var playMenuItem: NSMenuItem!
+    @IBOutlet weak var playNextMenuItem: NSMenuItem!
+    @IBOutlet weak var copyLinkMenuItem: NSMenuItem!
+    @IBOutlet weak var removeFromPlaylistMenuItem: NSMenuItem!
+    @IBOutlet weak var newPlaylistMenuItem: NSMenuItem!
+    @IBOutlet weak var addToPlaylistMenuItem: NSMenuItem!
+    @IBOutlet weak var addToPlaylistMenu: NSMenu!
+    
+    var playlistMenuUpdateID = ""
+    
+    @IBAction func menuItemAction(_ sender: NSMenuItem) {
+        guard let tableView = trackTableViewController()?.tableView else {
+            return
+        }
+        let selectedIndexs = tableView.selectedIndexs()
+        
+        switch sender {
+        case playMenuItem:
+            break
+        case playNextMenuItem:
+            break
+        case copyLinkMenuItem:
+            if let t = tracks.enumerated().filter ({
+                selectedIndexs.contains($0.offset)
+            }).first?.element {
+                let str = "https://music.163.com/song?id=\(t.id)"
+                ViewControllerManager.shared.copyToPasteboard(str)
+            }
+        case removeFromPlaylistMenuItem:
+            let selectedTracks = tracks.enumerated().filter {
+                selectedIndexs.contains($0.offset)
+            }
+            let ids = selectedTracks.map {
+                $0.element.id
+            }
+            let playlistId = self.playlistId
+            
+            switch playlistType {
+            case .discoverPlaylist:
+                guard let track = selectedTracks.first else { return }
+                PlayCore.shared.api.discoveryRecommendDislike(track.element.id).done {
+                    guard let newTrack = $0.0 else { return }
+                    newTrack.index = track.element.index
+                    self.tracks[track.offset] = newTrack
+                    print("Remove \(ids) from discoverPlaylist done.")
+                }.catch {
+                    if let er = ($0 as? NeteaseMusicAPI.RequestError) {
+                        switch er {
+                        case .errorCode(let code, let msg):
+                            if code == 432, msg == "今日暂无更多推荐" {
+                                return
+                            }
+                        default:
+                            break
+                        }
+                    }
+                    print("Remove \(ids) from discoverPlaylist error \($0).")
+                }
+            case .fmTrash:
+                guard let track = selectedTracks.first else { return }
+                PlayCore.shared.api.fmTrash(id: track.element.id, 0, false).done {
+                    let todo = "reload fmtrash"
+                    
+                }.catch {
+                    print("FM Trash Del error: \($0)")
+                }
+            default:
+                PlayCore.shared.api.playlistTracks(add: false, ids, to: playlistId).done {
+                    print("Remove \(ids) from playlist \(playlistId) done.")
+                    //                    self.initPlaylist(playlistId)
+                }.catch {
+                    print("Remove \(ids) from playlist \(playlistId) error \($0).")
+                }
+            }
+        case newPlaylistMenuItem:
+            guard let newPlaylistVC = newPlaylistViewController else { return }
+            self.presentAsSheet(newPlaylistVC)
+        default:
+            let playlistId = sender.tag
+            guard playlistId > 0 else { return }
+            
+            let ids = tracks.enumerated().filter {
+                selectedIndexs.contains($0.offset)
+            }.map {
+                $0.element.id
+            }
+            
+            PlayCore.shared.api.playlistTracks(add: true, ids, to: playlistId).done {
+                print("Add \(ids) to playlist \(playlistId) done.")
+                if playlistId == self.playlistId {
+                    //                    self.initPlaylist(playlistId)
+                }
+            }.catch {
+                print("Add \(ids) to playlist \(playlistId) error \($0).")
+            }
+        }
+    }
+    
+    lazy var newPlaylistViewController: NewPlaylistViewController? = {
+        let sb = NSStoryboard(name: "NewPlaylist", bundle: nil)
+        let vc = sb.instantiateController(withIdentifier: "NewPlaylistViewController") as? NewPlaylistViewController
+        vc?.updateSidebarItems = { [weak self] in
+            (self?.view.window?.windowController as? MainWindowController)?.initSidebarItems()
+        }
+        return vc
+    }()
+    
     
     var sidebarItemObserver: NSKeyValueObservation?
     var tracks: [Track] {
@@ -94,6 +205,7 @@ class PlaylistViewController: NSViewController {
                 self?.playlistType = newValue.type
                 self?.trackTableViewController()?.playlistId = id
                 self?.trackTableViewController()?.playlistType = newValue.type
+                self?.trackTableViewController()?.tableView.menu = self?.playlistMenu
             default:
                 return
             }
@@ -138,6 +250,8 @@ class PlaylistViewController: NSViewController {
         subscribeButton.isHidden = typeList.contains(playlistType)
         subscribeButton.isEnabled = true
         descriptionStackView.isHidden = playlistType == .topSongs
+        
+        initMenuItems()
     }
     
     func initPlaylist(_ id: Int) {
@@ -215,6 +329,17 @@ class PlaylistViewController: NSViewController {
         }
     }
     
+    func initMenuItems() {
+        var typeList = [SidebarViewController.ItemType]()
+        
+        playMenuItem.isHidden = playlistType == .fmTrash
+        playNextMenuItem.isHidden = playlistType == .fmTrash
+        copyLinkMenuItem.isHidden = false
+        typeList = [.subscribedPlaylist, .discoverPlaylist, .album, .topSongs]
+        removeFromPlaylistMenuItem.isHidden = typeList.contains(playlistType)
+        addToPlaylistMenuItem.isHidden = playlistType == .fmTrash
+    }
+    
     func trackTableViewController() -> TrackTableViewController? {
         let vc = children.compactMap {
             $0 as? TrackTableViewController
@@ -224,5 +349,90 @@ class PlaylistViewController: NSViewController {
     
     deinit {
         sidebarItemObserver?.invalidate()
+    }
+}
+
+extension PlaylistViewController: NSMenuItemValidation, NSMenuDelegate {
+    func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        guard let tableView = trackTableViewController()?.tableView else {
+            return false
+        }
+        let selectedIndexs = tableView.selectedIndexs()
+        
+        // Playlist items
+        if menuItem.tag != 0, selectedIndexs.count > 0 {
+            return true
+        }
+        
+        switch menuItem {
+        case copyLinkMenuItem:
+            return selectedIndexs.count == 1
+        case playMenuItem, playNextMenuItem:
+            return selectedIndexs.count > 0
+        case removeFromPlaylistMenuItem:
+            switch playlistType {
+            case .createdPlaylist, .favourite, .fmTrash:
+                return selectedIndexs.count > 0
+            case .discoverPlaylist:
+                return selectedIndexs.count == 1
+            default:
+                return false
+            }
+        case newPlaylistMenuItem:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    func menuDidClose(_ menu: NSMenu) {
+        playlistMenuUpdateID = ""
+    }
+    
+    func menuNeedsUpdate(_ menu: NSMenu) {
+        let markID = UUID().uuidString
+        playlistMenuUpdateID = markID
+
+        // Update playlist
+        addToPlaylistMenu.items.enumerated().forEach {
+            if $0.offset >= 2 {
+                addToPlaylistMenu.removeItem($0.element)
+            }
+        }
+        
+        PlayCore.shared.api.userPlaylist().map { itmes -> [NSMenuItem] in
+            guard markID == self.playlistMenuUpdateID else {
+                return []
+            }
+            
+            return itmes.enumerated().compactMap { i -> NSMenuItem? in
+                if i.element.subscribed {
+                    return nil
+                }
+                
+                var name = i.element.name
+                if i.offset == 0, name.contains("喜欢的音乐") {
+                    name = "我喜欢的音乐"
+                }
+                let item = NSMenuItem(title: name,
+                                      action: #selector(self.menuItemAction), keyEquivalent: "")
+                item.tag = i.element.id
+                return item
+            }
+            }.done(on: .main) {
+                $0.forEach {
+                    self.addToPlaylistMenu.addItem($0)
+                }
+            }.catch {
+                print($0)
+        }
+
+        switch playlistType {
+        case .discoverPlaylist:
+            // switch to not interested
+            removeFromPlaylistMenuItem.title = "Not Interested"
+        default:
+            removeFromPlaylistMenuItem.title = "Remove from Playlist"
+        }
     }
 }
