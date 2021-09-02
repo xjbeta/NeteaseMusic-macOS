13206cd9428e1be8f9c50687cf53972f2ac728d9
xjbeta <xjbeta.tiny@gmail.com>
2021-09-02 19:53:44 +0800

    Remove window toolbar.

--- NeteaseMusic/View/MainWindowController.swift
+++ NeteaseMusic/View/MainWindowController.swift
@@ -9,50 +9,12 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
-
-    @IBOutlet weak var searchField: NSSearchField!
-    
-    @IBAction func search(_ sender: NSSearchField) {
-        let str = sender.stringValue
-        ViewControllerManager.shared.searchFieldString = str
-        if str.isEmpty {
-            popover.close()
-            return
-        }
-        
-        PlayCore.shared.api.searchSuggest(str).done { [weak self] in
-            guard str == self?.searchField.stringValue else { return }
-            self?.searchSuggestionsVC.suggestResult = $0
-            self?.searchSuggestionsVC.initViewHeight(self?.popover)
-            }.catch {
-                print($0)
-        }
-        
-        guard !popover.isShown else { return }
-        popover.show(relativeTo: sender.frame, of: sender, preferredEdge: .minY)
-    }
-    
-    @IBOutlet weak var userButton: NSButton!
-    
     var updateLoginStatusObserver: NSObjectProtocol?
     var initSidebarPlaylistsObserver: NSObjectProtocol?
-    
-    let searchSuggestionsVC = NSStoryboard(name: .init("SearchSuggestionsView"), bundle: nil).instantiateController(withIdentifier: .init("SearchSuggestionsViewController")) as! SearchSuggestionsViewController
-    
-    lazy var popover: NSPopover = {
-        let p = NSPopover()
-        searchSuggestionsVC.dismissPopover = {
-            p.close()
-        }
-        p.behavior = .semitransient
-        p.contentViewController = searchSuggestionsVC
-        return p
-    }()
     
     override func windowDidLoad() {
         super.windowDidLoad()
         window?.isMovableByWindowBackground = true
-        userButton.isHidden = true
         
         updateLoginStatusObserver = NotificationCenter.default.addObserver(forName: .updateLoginStatus, object: nil, queue: .main) {
             guard let kv = $0.userInfo as? [String: Any],
@@ -94,23 +56,23 @@ class MainWindowController: NSWindowController {
             vc.children.compactMap {
                 $0 as? LoginViewController
                 }.first?.initViews()
-            userButton.isHidden = true
+//            userButton.isHidden = true
         }
     }
     
     func initUserButton() {
-        userButton.isHidden = true
-        PlayCore.shared.api.userInfo().done(on: .main) {
-            ViewControllerManager.shared.userId = $0.userId
-            self.userButton.title = $0.nickname
-            guard let u = $0.avatarImage else { return }
-            ImageLoader.image(u, true, 13)
-                .roundCorner(radius: .point(self.userButton.frame.size.width / 8))
-                .set(to: self.userButton)
-            self.userButton.isHidden = false
-            }.catch {
-                print($0)
-        }
+//        userButton.isHidden = true
+//        PlayCore.shared.api.userInfo().done(on: .main) {
+//            ViewControllerManager.shared.userId = $0.userId
+//            self.userButton.title = $0.nickname
+//            guard let u = $0.avatarImage else { return }
+//            ImageLoader.image(u, true, 13)
+//                .roundCorner(radius: .point(self.userButton.frame.size.width / 8))
+//                .set(to: self.userButton)
+//            self.userButton.isHidden = false
+//            }.catch {
+//                print($0)
+//        }
     }
     
     func initSidebarItems() {
