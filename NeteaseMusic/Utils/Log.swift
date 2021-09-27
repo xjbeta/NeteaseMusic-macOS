208c82270cdf94a5d1fe055cf600e896bad3a0e9
xjbeta <xjbeta.tiny@gmail.com>
2021-09-27 19:23:23 +0800

    Log.

--- NeteaseMusic/Utils/Log.swift
+++ NeteaseMusic/Utils/Log.swift
@@ -9,49 +9,73 @@
 import Cocoa
 import CocoaLumberjackSwift
 
-class Log: NSObject {
-    static func initLog() {
+
+enum Log {
+    static func setUp() {
         DDLog.add(DDOSLogger.sharedInstance) // Uses os_log
+        DDOSLogger.sharedInstance.logFormatter = LogFormatter()
+        
         if let logger = DDTTYLogger.sharedInstance {
             DDLog.add(logger)
+            logger.logFormatter = LogFormatter()
         }
-        
         
         let fileLogger = DDFileLogger() // File Logger
         
-        
+        fileLogger.doNotReuseLogFiles = true
         fileLogger.rollingFrequency = 60 * 60 * 24 // 24 hours
-        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 25
+        
         DDLog.add(fileLogger)
+        fileLogger.logFormatter = LogFormatter()
+        
+    }
 
+    @inlinable
+    static func verbose(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+       DDLogVerbose(message(), file: file, function: function, line: line)
+    }
+
+    @inlinable
+    static func debug(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+       DDLogDebug(message(), file: file, function: function, line: line)
+    }
+
+    @inlinable
+    static func info(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+       DDLogInfo(message(), file: file, function: function, line: line)
+    }
+
+    @inlinable
+    static func warn(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+       DDLogWarn(message(), file: file, function: function, line: line)
+    }
+
+    @inlinable
+    static func error(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+       DDLogError(message(), file: file, function: function, line: line)
+    }
+}
+
+class LogFormatter: DDDispatchQueueLogFormatter {
+    let dateFormatter: DateFormatter
     
-//        DDLogVerbose("Verbose")
-//        DDLogDebug("Debug")
-//        DDLogInfo("Info")
-//        DDLogWarn("Warn")
-//        DDLogError("Error")
+    override init() {
+        dateFormatter = DateFormatter()
+        dateFormatter.formatterBehavior = .behavior10_4
+        dateFormatter.dateFormat = "HH:mm:ss:SSS"
         
+        super.init()
+    }
+    
+    override func format(message logMessage: DDLogMessage) -> String? {
+        let dateAndTime = dateFormatter.string(from: logMessage.timestamp)
         
-    }
-    
-    
-    static func verbose(_ item: Any) {
-        DDLogVerbose(item)
-    }
-    
-    static func debug(_ item: Any) {
-        DDLogDebug(item)
-    }
-    
-    static func info(_ item: Any) {
-        DDLogInfo(item)
-    }
-    
-    static func warn(_ item: Any) {
-        DDLogWarn(item)
-    }
-    
-    static func error(_ item: Any) {
-        DDLogError(item)
+        var file = [logMessage.fileName, "\(logMessage.line)"]
+        if let f = logMessage.function {
+            file.append(f)
+        }
+        
+        return "\(dateAndTime) [\(file.joined(separator: ":"))]: \(logMessage.message)"
     }
 }
